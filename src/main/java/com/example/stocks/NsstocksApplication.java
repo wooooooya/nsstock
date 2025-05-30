36e8c6aa89e022f595ec810c81bf2a72162524b5
YyoungsY <diddud1236@daum.net>
2025-05-30 14:46:36 +0900

    no message

--- src/main/java/com/example/stocks/NsstocksApplication.java
+++ src/main/java/com/example/stocks/NsstocksApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NsstocksApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NsstocksApplication.class, args);
 	}
-
 }
